54c6d16b228e36c92f6dd1e295e0c77ce32613a5
Areian <iantyrho13@naver.com>
2024-04-19 22:14:20 +0900

    feat: 태그 전체 조회, 태그 조회 api 작성 #6

--- src/main/java/com/clipstory/clipstoryserver/domain/Member.java
+++ src/main/java/com/clipstory/clipstoryserver/domain/Member.java
@@ -1,5 +1,7 @@
 package com.clipstory.clipstoryserver.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
--- src/main/java/com/clipstory/clipstoryserver/service/TagService.java
+++ src/main/java/com/clipstory/clipstoryserver/service/TagService.java
@@ -3,8 +3,13 @@ package com.clipstory.clipstoryserver.service;
 import com.clipstory.clipstoryserver.domain.Member;
 import com.clipstory.clipstoryserver.domain.Movie;
 import com.clipstory.clipstoryserver.domain.Tag;
+import com.clipstory.clipstoryserver.global.response.GeneralException;
+import com.clipstory.clipstoryserver.global.response.Status;
 import com.clipstory.clipstoryserver.repository.TagRepository;
 import java.time.LocalDateTime;
+import java.util.List;
+
+import com.clipstory.clipstoryserver.responseDto.TagResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +22,23 @@ public class TagService {
     public void createTag(Member member, Movie movie, String content, LocalDateTime createdAt) {
         Tag tag = Tag.toEntity(member, movie, content, createdAt);
         tagRepository.save(tag);
+    }
+
+    public TagResponseDto getTag(Long tagId) {
+        Tag tag = getTagById(tagId);
+        return TagResponseDto.toResponseDto(tag);
+    }
+
+    public Tag getTagById(Long tagId) {
+        return tagRepository.findById(tagId)
+                .orElseThrow(() -> new GeneralException(Status.TAG_NOT_FOUND));
+    }
+
+    public List<TagResponseDto> getAllTag() {
+        List<Tag> tags = tagRepository.findAll();
+        return tags.stream()
+                .map(tag -> TagResponseDto.toResponseDto(tag))
+                .toList();
     }
 
 }
