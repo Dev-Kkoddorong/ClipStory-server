15c8dbeb62fa0e4b369435ffe72b8437fc482448
mingmingmon <mingmingmon@naver.com>
2024-04-20 21:50:00 +0900

    feat: 장르 전체 조회 api 작성 #12

--- src/main/java/com/clipstory/clipstoryserver/service/GenreService.java
+++ src/main/java/com/clipstory/clipstoryserver/service/GenreService.java
@@ -2,6 +2,9 @@ package com.clipstory.clipstoryserver.service;
 
 import com.clipstory.clipstoryserver.domain.Genre;
 import com.clipstory.clipstoryserver.repository.GenreRepository;
+import com.clipstory.clipstoryserver.responseDto.GenreResponseDto;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,13 @@ public class GenreService {
         return genreRepository.findByName(name).orElseGet(
                 () -> genreRepository.save(Genre.toEntity(name))
         );
+    }
+
+    public List<GenreResponseDto> getAllGenres() {
+        List<Genre> genres = genreRepository.findAll();
+        return genres.stream()
+                .map(genre -> GenreResponseDto.toGenreResponseDto(genre))
+                .collect(Collectors.toList());
     }
 
 }
